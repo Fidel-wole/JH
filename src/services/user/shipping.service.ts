b1ca953e12ff136599d01cf29191a565a4629410
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-05-30 18:07:25 +0100

    feat: shipping address endpoint

--- src/services/user/shipping.service.ts
+++ src/services/user/shipping.service.ts
@@ -1,19 +1,34 @@
-import ShippingAddress from "../../models/shippingAddress";
+import ShippingAddress from '../../models/shippingAddress';
 
-export default class ShippingService{
-    static async addShippingAddress(data:any){
-        try{
-            return await ShippingAddress.create(data)
-        }catch(err:any){
-            throw err
-        }
+export default class ShippingService {
+  static async addShippingAddress(data: any) {
+    try {
+      let address = await ShippingAddress.findOne({ userId: data.userId });
+      if (address) {
+        address.address.push({
+          street: data.address[0].street,
+          building: data.address[0].building,
+          landmark: data.address[0].landmark,
+          city: data.address[0].city,
+          state: data.address[0].state,
+          postalCode: data.address[0].postalCode,
+          country: data.address[0].country,
+        });
+        return address;
+      } else {
+        address = await ShippingAddress.create(data);
+      }
+      return address;
+    } catch (err: any) {
+      throw err.message;
     }
+  }
 
-    static async getShippingAddress(userId:any){
-        try{
-            return await ShippingAddress.find(userId)
-        }catch(err:any){
-            throw err
-        }
+  static async getShippingAddress(userId: any) {
+    try {
+      return await ShippingAddress.find(userId);
+    } catch (err: any) {
+      throw err;
     }
+  }
 }
