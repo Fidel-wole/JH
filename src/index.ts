555cf24db8fbfc4b6f0cd323ea3d057ee00f068a
Fidelis Adewole <adewoyeadedayo10@gmail.com>
2024-05-31 06:06:57 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,7 @@ import rateLimit from "express-rate-limit";
 dotenv.config();
 
 const app = express();
-app.set('trust proxy', false);
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.'
-});
-
-app.use(limiter);
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
